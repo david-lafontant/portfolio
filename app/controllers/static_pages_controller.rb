bb50aa3b6a27721316810d9780ea485d464419cd
david-lafontant <lafontant.david@gmail.com>
2022-08-10 12:46:16 -0600

    Remove new and create actions in static_pages controller

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -8,24 +8,6 @@ class StaticPagesController < ApplicationController
     @profile_data
   end
 
-  def new
-    @contact = Contact.new
-  end
-
-  def thanks; end
-
-  def create
-    @contact = Contact.new(params[:contact])
-    @contact.request = request
-    if @contact.deliver
-      flash.now[:success] = 'Message sent!'
-      redirect_to contacts_thanks_url
-    else
-      flash.now[:error] = 'Could not send message'
-      render :new
-    end
-  end
-
   def about; end
 
   def home; end
