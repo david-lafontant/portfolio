ed82f6eb990a9a1a7f891238e7ec9b6bb7ca5186
david-lafontant <lafontant.david@gmail.com>
2022-08-10 11:56:23 -0600

    Fix linter errors

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,6 @@
 class StaticPagesController < ApplicationController
   require 'json'
-  
+
   def project
     @profile_data = JSON.parse(File.read('app/assets/javascripts/data.json'))
     @profile_data = @profile_data['data']['repositoryOwner']['itemShowcase']['items']['edges']
@@ -26,11 +26,9 @@ class StaticPagesController < ApplicationController
     end
   end
 
-  def about;
-  end
+  def about; end
 
-  def home;
-  end
+  def home; end
 
   def resume
     send_file "#{Rails.root}/app/assets/docs/Resume.pdf", type: 'application/pdf', x_sendfile: true
