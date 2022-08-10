3d22429488703edcde943b4ac266650f94abfeac
david-lafontant <lafontant.david@gmail.com>
2022-08-10 17:14:21 -0600

    Update SMTP parameter

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,7 +96,7 @@ Rails.application.configure do
     :address        => ENV['MAILGUN_SMTP_SERVER'],
     :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
     :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => 'warm-depths-77428.herokuapp.com', # UPDATE THIS VALUE WITH YOUR OWN APP
+    :domain         => 'protected-lake-38594.herokuapp.com', # UPDATE THIS VALUE WITH YOUR OWN APP
     :authentication => :plain,
   }
   ActionMailer::Base.delivery_method = :smtp
