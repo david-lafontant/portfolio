2a63ce089a1ac51e38e27b8a896843291ffc43f9
david-lafontant <lafontant.david@gmail.com>
2022-08-10 08:56:17 -0600

    Add static pages

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,19 @@
+class StaticPagesController < ApplicationController
+  def project
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def about
+  end
+
+  def home
+  end
+
+  def resume
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'static_pages/project'
+  get 'static_pages/new'
+  get 'static_pages/create'
+  get 'static_pages/about'
+  get 'static_pages/home'
+  get 'static_pages/resume'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
