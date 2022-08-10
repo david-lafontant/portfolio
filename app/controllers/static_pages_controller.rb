a5eeff5be88cf2128bef6bb3367259f93f4ec0c4
david-lafontant <lafontant.david@gmail.com>
2022-08-10 11:33:04 -0600

    Update actions in controllers

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,19 +1,38 @@
 class StaticPagesController < ApplicationController
+  require 'json'
+  
   def project
+    @profile_data = JSON.parse(File.read('app/assets/javascripts/data.json'))
+    @profile_data = @profile_data['data']['repositoryOwner']['itemShowcase']['items']['edges']
+
+    @profile_data
   end
 
   def new
+    @contact = Contact.new
   end
+
+  def thanks; end
 
   def create
+    @contact = Contact.new(params[:contact])
+    @contact.request = request
+    if @contact.deliver
+      flash.now[:success] = 'Message sent!'
+      redirect_to contacts_thanks_url
+    else
+      flash.now[:error] = 'Could not send message'
+      render :new
+    end
   end
 
-  def about
+  def about;
   end
 
-  def home
+  def home;
   end
 
   def resume
+    send_file "#{Rails.root}/app/assets/docs/Resume.pdf", type: 'application/pdf', x_sendfile: true
   end
 end
